4bce52dd12ab4f0424749d6555dcf12dcf85676c
Maykon Menezes <maykon.meneses@gmail.com>
2019-12-16 15:45:29 -0300

    model, view and controller included for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,36 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = current_user.contacts
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    user = User.find_by(email: contact_params[:record][:email])
+    @contact = current_user.contacts.build(record: user)
+    if @contact.save
+      flash[:notice] = "Contact saved successfully"
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @contact = current_user.contacts.find(params[:id])
+    if @contact.destroy
+      flash[:notice] = "Contact destroyed successfully"
+    else
+      flash[:alert] = "An error acurred when trying to remove this contact"
+    end
+    redirect_to contacts_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(record: :email)
+  end
 end
